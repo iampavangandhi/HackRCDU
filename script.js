bc882b56487d0cdb1ab9bbadb005cee953d67dd7
Pavan Gandhi <pavangandhi100@gmail.com>
2020-03-11 11:59:28 +0530

    files updated

--- script.js
+++ script.js
@@ -8,16 +8,20 @@ let countDown = new Date("Feb 27, 2020 09:00:00").getTime(),
     let now = new Date().getTime(),
       distance = countDown - now;
 
-    (document.getElementById("days").innerText = Math.floor(distance / day)),
-      (document.getElementById("hours").innerText = Math.floor(
-        (distance % day) / hour
-      )),
-      (document.getElementById("minutes").innerText = Math.floor(
-        (distance % hour) / minute
-      )),
-      (document.getElementById("seconds").innerText = Math.floor(
-        (distance % minute) / second
-      ));
+    (document.getElementById("days").innerText =
+      Math.floor(distance / day) > 0 ? Math.floor(distance / day) : 0),
+      (document.getElementById("hours").innerText =
+        Math.floor((distance % day) / hour) > 0
+          ? Math.floor((distance % day) / hour)
+          : 0),
+      (document.getElementById("minutes").innerText =
+        Math.floor((distance % hour) / minute) > 0
+          ? Math.floor((distance % hour) / minute)
+          : 0),
+      (document.getElementById("seconds").innerText =
+        Math.floor((distance % minute) / second) > 0
+          ? Math.floor((distance % minute) / second)
+          : 0);
 
     //do something later when date is reached
     //if (distance < 0) {
@@ -25,3 +29,12 @@ let countDown = new Date("Feb 27, 2020 09:00:00").getTime(),
     //  'IT'S COUNTED DOWN!;
     //}
   }, second);
+
+const items = document.querySelectorAll(".accordion a");
+
+function toggleAccordion() {
+  this.classList.toggle("active");
+  this.nextElementSibling.classList.toggle("active");
+}
+
+items.forEach(item => item.addEventListener("click", toggleAccordion));
